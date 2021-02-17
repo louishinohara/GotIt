c2a87507dd668f03bbd89cbf1ddaecfddbff9ac2
Louis Shinohara <louishinohara@gmail.com>
2021-02-17 18:35:43 -0500

    update postToZoom
    Speed up posting to <1 second

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from speak import speak
 from getAudio import getAudioFromMic
 from getAnalysis import getAnalysis
 from postToZoom import postTranscript
+import requests
+import time
 
 def main():
-    # findInputSource()
+    findInputSource()
     # speak('Play Recording Now')
     keepGoing = True
-    api_token = 'https://wmcc.zoom.us/closedcaption?id=99211570681&ns=VVNISU8gU0hJTk9IQVJBJ3MgWm9vbSBNZWV0aW5n&expire=86400&sparams=id%2Cns%2Cexpire&signature=xhouuJ1_oVm6g6zo1qZ0-1umW37xxtR-OQ8wJ83uINA.AG.Qurhu1AvwaiKL0Zb4SsIH6Y6d68Eotn6eIPJUJzeq6xmpF4G2NQX8Cl35bibNM7ESoyd2wtz86bc1y5BkpvUp7YheOFGchJTzYP3m7NAqUNQkDCmMhU30OWn.JPDT-e80tIuPkixHP55cuw.wV4vT0rfh9GRYsV8'
+    api_token = 'https://wmcc.zoom.us/closedcaption?id=99766787474&ns=VVNISU8gU0hJTk9IQVJBJ3MgWm9vbSBNZWV0aW5n&expire=86400&sparams=id%2Cns%2Cexpire&signature=7PkopQpxFgXxIfGtUx9_1PfHVFWywC_Ni0t1eYgFhjA.AG.HG97mnkVM26s8kceuEeukAf8gD1WMPZQ1_4GJPdlp41JrSo1VF1D3oeiwBpREdhUtPiWHkddldO00IiqaLFDwuugFs87YUTR-AGQrL_k0WeI77c4OdOskkwC.5VVWewsvw6jVetJG4WPiyQ.DPus7C6RE0mLTNNK'
 
-    for i in range(5):
+
+    session = requests.Session()
+    seqNum = 1
+    for i in range(10):
         text = getAudioFromMic()
-        getAnalysis(text)
-        postTranscript(api_token, text)
+        print(text)
+        # getAnalysis(text)
+        # text = 'Text Number'
+        # textToSend = text+ ': ' + str(seqNum)
+        postTranscript(api_token, text, session, seqNum)
+        seqNum += 1
+        time.sleep(1.2)
     
 
 main()

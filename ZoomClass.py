a643468e49b270c95bb20cdfd8a2b5f4e8a0d2ab
Louis Shinohara <louishinohara@gmail.com>
2021-02-17 21:45:19 -0500

    Separate all code into classes

---
+++ ZoomClass.py
@@ -0,0 +1,24 @@
+import time
+import requests
+
+# https://github.com/heyyeh12/zoom_cc#development
+
+class Zoom:
+    def __init__(self, apiToken):
+        self.seq_num = 0                                                # Acts like a key when sending HTTPS Posts
+        self.api_token = apiToken
+        self.session = requests.Session()                               # Keeps open session
+        self.params = {'seq' : str(0), 'lang' : "en-US" }               # For POST
+        self.headers = {'Content-type': 'text/plain; charset=utf-8'}    # For POST
+
+    def increase_seq_num(self):                                         # Increase the seq number and params for POST
+        self.seq_num += 1
+        self.params = {
+        'seq' : str(self.seq_num),
+        'lang' : "en-US"
+        }
+    
+    def post_transcript(self, transcript):
+        self.session.post(self.api_token, params=self.params, data=transcript, headers=self.headers)    # Post transcript
+        self.increase_seq_num()     # Increment Seq Num
+        time.sleep(1.5)             # Sleep so the API is not overloaded with requests
--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-from findInputSource import findInputSource
-from speak import speak
-from getAudio import getAudioFromMic
-from getAnalysis import getAnalysis
-from postToZoom import postTranscript
-import requests
-import time
+
+from ZoomClass import Zoom
+from VoiceClass import Voice
 
 def main():
-    findInputSource()
-    # speak('Play Recording Now')
-    keepGoing = True
-    api_token = 'https://wmcc.zoom.us/closedcaption?id=99766787474&ns=VVNISU8gU0hJTk9IQVJBJ3MgWm9vbSBNZWV0aW5n&expire=86400&sparams=id%2Cns%2Cexpire&signature=7PkopQpxFgXxIfGtUx9_1PfHVFWywC_Ni0t1eYgFhjA.AG.HG97mnkVM26s8kceuEeukAf8gD1WMPZQ1_4GJPdlp41JrSo1VF1D3oeiwBpREdhUtPiWHkddldO00IiqaLFDwuugFs87YUTR-AGQrL_k0WeI77c4OdOskkwC.5VVWewsvw6jVetJG4WPiyQ.DPus7C6RE0mLTNNK'
+    voice = Voice()
 
 
-    session = requests.Session()
-    seqNum = 1
-    for i in range(10):
-        text = getAudioFromMic()
-        print(text)
-        # getAnalysis(text)
-        # text = 'Text Number'
-        # textToSend = text+ ': ' + str(seqNum)
-        postTranscript(api_token, text, session, seqNum)
-        seqNum += 1
-        time.sleep(1.2)
+    api_token = 'https://wmcc.zoom.us/closedcaption?id=94627603448&ns=VVNISU8gU0hJTk9IQVJBJ3MgWm9vbSBNZWV0aW5n&expire=86400&sparams=id%2Cns%2Cexpire&signature=c1v4xoLvBdDXgT7pasbJ0KBx05SaajvYMTd-ctsZyb8.AG.SFklqR-FQajIVfAd30QcJPLS8DlDEyjrHiZTkoBoFjbZAWbqAIoAknE4oTscOssnNlxYutnsE43qh8n-0ZY8ZUG_CQ82nqn4IT0TGDtjN6QrRNHHoSuqrIs8.t8sHI_c-P3vhspDVeCNzRQ.AwLua5mWe3uMJGuJ'
     
+    zoom = Zoom(api_token)
+
+    for i in range(10):
+        text = voice.getAudioFromMic()
+        zoom.post_transcript(text)
 
 main()
+
+
+# Other possible methods from Voice class
+    # voice.findInputSource()
+    # voice.speak('Play Recording Now')
+    # voice.analyzeText(text)

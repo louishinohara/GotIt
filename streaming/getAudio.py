1e294fcda9844f3682c3f92aecc04a520f9ceaea
Suraj <55250165+SurajSanyal@users.noreply.github.com>
2021-03-03 19:46:49 -0500

    update gcloud-streaming

---
+++ streaming/getAudio.py
@@ -0,0 +1,26 @@
+import os
+import time
+import speech_recognition as sr
+import pyaudio
+
+def getAudioFromMic():   # Gets audio from mic and translates to text
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=2)
+    # mic = sr.Microphone(device_index=None)
+
+
+    with mic as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+
+
